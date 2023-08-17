e99610b9b0a05f435fc109f67712ad2f8db70bfa
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2023-08-17 20:05:46 +0200

    uncomment GameModeBase (hope this works now)

--- UEcppExercises/Source/UEcppExercises/UEcppExercisesGameModeBase.cpp
+++ UEcppExercises/Source/UEcppExercises/UEcppExercisesGameModeBase.cpp
@@ -3,16 +3,16 @@
 
 #include "UEcppExercisesGameModeBase.h"
 
-/*void AUEcppExercisesGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+void AUEcppExercisesGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
 {
-	//Super::InitGame(MapName, Options, ErrorMessage);
+	Super::InitGame(MapName, Options, ErrorMessage);
 
-	//StatueManager = GetWorld()->SpawnActor<AStatueManager>();
-	//StatueManager->Initialize(StatueClass);
-}*/
+	StatueManager = GetWorld()->SpawnActor<AStatueManager>();
+	StatueManager->Initialize(StatueClass);
+}
 
 void AUEcppExercisesGameModeBase::SpawnStatue(const FVector Location)
 {
-	//StatueManager->SpawnStatue(Location);
+	StatueManager->SpawnStatue(Location);
 }
 
